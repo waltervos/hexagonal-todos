d451b550b628650f002e9b2fe4a894bbe253d1d8
Walter Vos <walter.vos@ns.nl>
2024-07-17 09:59:05 +0200

    Initial commit

---
+++ driving_adapters/tests/hexagonal-todos_spec.py
@@ -0,0 +1,7 @@
+class DescribeHexagonalTodos:
+    def it_should_fail_this_test(self):
+        """
+        Failing this test confirms that everything is working as expected. Delete this test when you start writing your own tests.
+        """
+        
+        assert True == False
